163dadc3892780348428083af7692d55964e2072
Lyly <khlbolatovna@gmail.com>
2022-05-24 19:20:59 +0600

    added funcs implementations

--- cache.go
+++ cache.go
@@ -3,21 +3,41 @@ package cache
 import "time"
 
 type Cache struct {
+	m map[string]string
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	k, ok := c.m[key]
+	if !ok {
+		return "", ok
+	}
+	return k, ok
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.m[key] = value
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	var keys []string
+
+	for key, _ := range c.m {
+		keys = append(keys, key)
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	startTime := time.Now()
+	res := startTime.Before(deadline)
+	if res {
+		time.Sleep(2 * time.Second)
+		c.m[key] = value
+		time.Sleep(2 * time.Second)
+	}
 }
